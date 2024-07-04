e6973725d39ae5eeb487c3a08f734243180e360c
vitalii-baidak <137405014+vitaliy-bdk-1x@users.noreply.github.com>
2024-07-04 20:43:32 +0300

    App init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!--
+    TODO: Add to readme file next info:
+      1. How to start project with npm
+      2. How to start project with docker
+      3. Vuetify components auto imported
+  -->
+
+  <v-app>
+    <v-main>
+      Hello!
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts"></script>

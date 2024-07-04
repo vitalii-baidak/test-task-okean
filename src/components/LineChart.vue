7a80a6898f6cdcbbadf85e81d324331ddbeebfb7
vitalii-baidak <137405014+vitaliy-bdk-1x@users.noreply.github.com>
2024-07-04 22:14:00 +0300

    Prepared line chart component

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,9 +1,51 @@
 <template>
   <v-card>
     <v-card-text>
-      Line chart
+      <p class="text-body-1 text-center mb-2">
+        {{ title }}
+      </p>
+
+      <div>
+        <Line
+          :data="chartData"
+          :options="chartConfig"
+        />
+      </div>
     </v-card-text>
   </v-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import { Line } from 'vue-chartjs';
+import { Chart, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip } from 'chart.js';
+
+Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip );
+
+// Props
+
+const props = defineProps<{
+  title: string
+  labels: string[]
+  values: number[]
+}>();
+
+// Chart
+
+const chartConfig = {
+  responsive: true,
+  maintainAspectRatio: false
+};
+
+const chartData = computed(() => {
+  return {
+    labels: props.labels,
+    datasets: [{
+      data: props.values,
+      tension: 0.1,
+      borderColor: 'rgb(75, 192, 192)',
+    }]
+  }
+});
+</script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container>
-    <LineChart />
+  <v-container class="dashboard-view-container">
+    <LineChart title="Sales" :labels="salesData.labels" :values="salesData.values" />
 
     <div class="mt-3">
       <BarChart />
@@ -9,6 +9,24 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import mockChartsData from '@/shared/mockChartsData.json';
+
 import BarChart from '@/components/BarChart.vue';
 import LineChart from '@/components/LineChart.vue';
+
+// Charts data
+
+const salesData = computed(() => {
+  return {
+    labels: mockChartsData.salesData.map(salesItem => salesItem.month),
+    values: mockChartsData.salesData.map(salesItem => salesItem.sales),
+  }
+});
 </script>
+
+<style scoped>
+.dashboard-view-container {
+  max-width: 1600px;
+}
+</style>

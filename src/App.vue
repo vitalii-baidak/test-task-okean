62e1ac00d9c113d1468ab7d1790e652a662f39b9
vitalii-baidak <137405014+vitaliy-bdk-1x@users.noreply.github.com>
2024-07-05 12:49:40 +0300

    Clearify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
 <template>
-  <!--
-    TODO: Add to readme file next info:
-      1. How to start project with npm
-      2. How to start project with docker
-      3. Vuetify components auto imported
-  -->
-
   <v-app>
     <v-main>
       <DashboardView />
@@ -14,5 +7,5 @@
 </template>
 
 <script setup lang="ts">
-import DashboardView from "@/views/DashboardView.vue";
+import DashboardView from "@/views/DashboґardView.vue";
 </script>
